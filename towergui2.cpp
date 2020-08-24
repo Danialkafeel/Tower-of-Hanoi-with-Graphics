8772cfa81229a778eb264274cefb3f6dfe860060
Danial Kafeel <danialkafeel@gmail.com>
2020-08-24 22:55:33 +0530

    Add files via upload

---
+++ towergui2.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+
+stack <int> peg[3];
+void begins()
+{
+      cleardevice();
+      stack <int> temp;
+
+     ///    DISKS
+     for(int j=0;j<3;j++)
+     {
+          while(!peg[j].empty())
+          {
+              temp.push(peg[j].top());
+              peg[j].pop();
+          }
+          int i=0;
+          while(!temp.empty())
+          {
+              if(temp.top()==1)
+              {
+                   rectangle(380+(250*j),370-i,440+(250*j),400-i);
+                    setfillstyle(9,YELLOW);
+                    floodfill(410+(250*j),385-i,WHITE);
+                   i+=30;
+              }
+              else if(temp.top()==2)
+              {
+                   rectangle(360+(250*j),370-i,460+(250*j),400-i);
+                   setfillstyle(9,RED);
+                    floodfill(410+(250*j),385-i,WHITE);
+                   i+=30;
+              }
+              else if(temp.top()==3)
+              {
+                   rectangle(330+(250*j),370-i,490+(250*j),400-i);
+                   setfillstyle(9,BLUE);
+                    floodfill(410+(250*j),385-i,WHITE);
+                   i+=30;
+              }
+              else if(temp.top()==4)
+              {
+                   rectangle(300+(250*j),370-i,520+(250*j),400-i);
+                   setfillstyle(9,GREEN);
+                    floodfill(410+(250*j),385-i,WHITE);
+                   i+=30;
+              }
+              else if(temp.top()==5)
+              {
+                   rectangle(290+(250*j),370-i,530+(250*j),400-i);
+                   setfillstyle(9,CYAN);
+                    floodfill(410+(250*j),385-i,WHITE);
+                   i+=30;
+              }
+              peg[j].push(temp.top());
+              temp.pop();
+          }
+     }
+     ///     PEGS
+        rectangle(405,200,415,400);
+        setcolor(WHITE);
+        settextstyle(ITALIC_FONTTYPE,HORIZ_DIR,100);
+        outtextxy(405,410,"A");
+        rectangle(655,200,665,400);
+        setcolor(WHITE);
+        settextstyle(ITALIC_FONTTYPE,HORIZ_DIR,100);
+        outtextxy(655,410,"B");
+        rectangle(905,200,915,400);
+        setcolor(WHITE);
+        settextstyle(ITALIC_FONTTYPE,HORIZ_DIR,100);
+        outtextxy(905,410,"C");
+}
+void draw(int i,int j)
+{
+    i--;
+    j--;
+    peg[j].push(peg[i].top());
+    peg[i].pop();
+    delay(800);
+    begins();
+}
+
+void solve(int num,int from, int to,int aux)
+{
+    if(num==1)
+    {
+        cout<<"MOVE DISK NO. "<<num<<" FROM "<<from<<" TO "<<aux<<endl;
+        draw(from,aux);
+        cout<<"MOVE DISK NO. "<<num<<" FROM "<<aux<<" TO "<<to<<endl;
+        draw(aux,to);
+        return;
+    }
+    solve(num-1,from,to,aux);
+    cout<<"MOVE DISK NO. "<<num<<" FROM "<<from<<" TO "<<aux<<endl;
+    draw(from,aux);
+    solve(num-1,to,from,aux);
+    cout<<"MOVE DISK NO. "<<num<<" FROM "<<aux<<" TO "<<to<<endl;
+    draw(aux,to);
+    solve(num-1,from,to,aux);
+}
+
+int main()
+{
+    int num;
+    cout<<"ENTER THE NO. OF DISKS"<<endl;
+    cin>>num;
+
+    for(int i=num;i>0;i--)
+        peg[0].push(i);
+
+    int gd=0,gm;
+    initgraph(&gd,&gm,"");
+    begins();
+    getch();
+    solve(num,1,2,3);
+    getch();
+    closegraph();
+    return 0;
+}
